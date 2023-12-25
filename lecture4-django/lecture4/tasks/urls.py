2bf031f8e4ae80622725383a42f62ccbf3a97d55
Sukumar Subudhi <suku.subudhi@gmail.com>
2023-12-25 08:27:08 +0530

    dynamic urls, added a html page for add tasks

--- lecture4-django/lecture4/tasks/urls.py
+++ lecture4-django/lecture4/tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = "tasks"
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="index"),
+    path('add', views.add_task, name="add")
 ]
--- lecture4-django/lecture4/tasks/views.py
+++ lecture4-django/lecture4/tasks/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
+from django.urls import reverse
 
 task_list = ["bring grocery", "bring some eggs"]
+
+
 
 class TaskForm(forms.Form):
     task = forms.CharField(max_length=255)
 
 # Create your views here.
 def index(request):
+    return render(request,template_name="tasks/index.html", context={"tasks_list": task_list})
+
+def add_task(request):
     form = TaskForm()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             new_task = form.cleaned_data['task']
             task_list.append(new_task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             form = TaskForm()
-    return render(request,template_name="tasks/index.html", context={"tasks_list": task_list, "form": form})
+    return render(request, template_name="tasks/add.html", context={"form": form})
